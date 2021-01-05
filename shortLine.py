24dccfa05ea9ff0054a759ecf4a77142a07ad67b
hereisch <694749274@qq.com>
2021-01-05 17:09:14 +0800

    first_ver

--- shortLine.py
+++ shortLine.py
@@ -14,21 +14,40 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 
 
-if __name__ == '__main__':
-
-    print(time.strftime("%Y-%m-%d", time.localtime()))
-    df = ts.get_realtime_quotes('000876')
-    print(df.columns.values.tolist())
-
-    df = ts.get_realtime_quotes('600685')  # Single stock symbol
+def realTime(code):
+    """
+    获取当日实时成交
+    :param code:
+    :return:
+    """
+    df = ts.get_realtime_quotes(code)  # Single stock symbol
     real = df[['code', 'name', 'price', 'bid', 'ask', 'volume', 'amount', 'time']]
     print(real.columns.values.tolist())
     while True:
-
-        df = ts.get_realtime_quotes('600685')  # Single stock symbol
+        df = ts.get_realtime_quotes(code)  # Single stock symbol
         real = df[['code', 'name', 'price', 'bid', 'ask', 'volume', 'amount', 'time']]
         print(list(real.iloc[0].values))
         # print(list(df.iloc[0].values))
         time.sleep(5)
+
+def todayQuotes(code):
+    """
+    获取当日历史分笔数据，复盘用
+    :param code:
+    :return:
+    """
+    df = ts.get_today_ticks(code)
+    print(df)
+
+
+
+if __name__ == '__main__':
+
+    code = input('股票代码：')
+    print(time.strftime("%Y-%m-%d", time.localtime()))
+    # df = ts.get_realtime_quotes(code)
+    # print(df.columns.values.tolist())
+    todayQuotes(code)
+
 
 
