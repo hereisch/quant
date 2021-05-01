060bc74553cc70eb2f10fc4faf0ce44949ac6b78
hereisch <694749274@qq.com>
2021-05-01 16:32:01 +0800

    Upd

--- selectStock.py
+++ selectStock.py
@@ -155,7 +155,6 @@ if __name__ == '__main__':
     s.download()
     s.topN()
     # s.uniqDayK()
-
     locale.setlocale(locale.LC_CTYPE, 'chinese')
     print(time.strftime('%Y年%m月%d日%H时%M分%S秒'))
 
