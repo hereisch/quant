2fd9ff5f48d25ff8ec6fd4f25be0e908257ed822
hereisch <694749274@qq.com>
2021-05-04 14:38:55 +0800

    Upd

--- test.py
+++ test.py
@@ -12,7 +12,9 @@ import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import plotly.offline as po
-
+import plotly
+import numpy as np
+import plotly.graph_objects as go
 
 pd.set_option('display.width', 5000)
 pd.set_option('display.max_rows', None)
@@ -23,13 +25,13 @@ client = pymongo.MongoClient(host="127.0.0.1", port=27017)
 db = client['quant']
 
 
-def can_vol(dataframe=None, start=80, end=180, name='Candlestick'):
-    import plotly
-    import numpy as np
-    import plotly.graph_objects as go
+def can_vol(dataframe=None, start=0, end=350, name='Candlestick'):
+
 
     data1 = dataframe.iloc[start:end, :]  # 区间，这里我只是测试，并没有真正用时间来选
     data1 = data1.sort_index(axis=0, ascending=True)
+    x_axis = [i[2:] for i in data1.index]
+    print(x_axis)
     # 生成新列，以便后面设置颜色
     data1['diag'] = np.empty(len(data1))
     # 设置涨/跌成交量柱状图的颜色
@@ -42,11 +44,11 @@ def can_vol(dataframe=None, start=80, end=180, name='Candlestick'):
     # layout的参数超级多，因为它用一个字典可以集成所有图的所有格式
     # 这个函数里layout值得注意的是 type='category'，设置x轴的格式不是candlestick自带的datetime形式，
     # 因为如果用自带datetime格式总会显示出周末空格，这个我找了好久才解决周末空格问题。。。
-    candle = go.Candlestick(x=data1.index,
+    candle = go.Candlestick(x=x_axis,
                             open=data1.open, high=data1.high,
                             low=data1.low, close=data1.close, increasing_line_color='#f6416c',
                             decreasing_line_color='#7bc0a3', name="Price")
-    vol = go.Bar(x=data1.index,
+    vol = go.Bar(x=x_axis,
                  y=data1.volume, name="Volume", marker_color=data1.diag, opacity=0.5, yaxis='y2')
     # 这里一定要设置yaxis=2, 确保成交量的y轴在右边，不和价格的y轴在一起
     data = [candle, vol]
